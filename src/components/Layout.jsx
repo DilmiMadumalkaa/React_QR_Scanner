72f0a4fbe904d3bd07f3bc2618674229259f733b
TimashaWanninyake <160386834+TimashaWanninyake@users.noreply.github.com>
2026-01-03 20:47:48 +0530

    Refactor Navbar component to include logout functionality and improve navigation links

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,29 +1,19 @@
-import React, { useState } from "react";
+import React from "react";
 import { useAuth } from "../services/authService";
 import Navbar from "./common/navbar";
-import Sidebar from "./Sidebar";
 
 export default function Layout({ children }) {
   const { user, logout } = useAuth();
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
   return (
     <div className="min-h-screen bg-white overflow-hidden relative text-gray-800">
       <div className="absolute inset-0 bg-gray-50/50"></div>
 
       {/* Fixed Navbar */}
-      <Navbar user={user} />
+      <Navbar user={user} logout={logout} />
 
-      {/* Fixed Sidebar */}
-      <Sidebar
-        user={user}
-        logout={logout}
-        isOpen={isSidebarOpen}
-        setIsOpen={setIsSidebarOpen}
-      />
-
-      {/* Main Content Area - Only this changes */}
-      <main className="pt-24 relative z-10 mx-5 lg:ml-64">
+      {/* Main Content Area */}
+      <main className="pt-24 relative z-10 mx-5">
         {children}
       </main>
     </div>
